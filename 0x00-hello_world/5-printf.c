89ce23da3cb9a2d2ac262c9d5b20bf0c252eccc6
IZERE <blaiseinnocenti@tgmail.com>
2024-07-01 18:01:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#icnlude <stdio.h>
+
+/**
+ * main - prints "with proper grammer, but the outcome is a piece of art," followed by newline
+ * Return: always 0.
+ *
+ */
+int main(void)
+
+{
+	printf("with proper grammer.but the outcome is a piece of art,\n"):
+	return (0);
+}
