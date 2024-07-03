2bc62fa8cd3636c118c42f2b4495de863f170ae2
IZERE <blaiseinnocenti@tgmail.com>
2024-07-02 19:34:30 -0700

    updates

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string
+ * _puts _ Prints a string
  * @str: The string to print
  * Return: void
  */
 void _puts(char *str)
 {
-	for(; *str != '\0'; str++)
+	for (; *str != '\0'; str++)
 	{
 		_putchar(*str);
 	}
