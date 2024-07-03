f1e4b5fd80b811402137db4b02c89700f25e1c65
IZERE <blaiseinnocenti@tgmail.com>
2024-07-02 18:43:33 -0700

    updates

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,8 +7,7 @@
  * Return: nothing
  */
 void swap_int(int *a, int *b)
-	/* the function that swaps the values of two integers.
-	 */
+	/* the function thta swaps the value of two integers. */
 {
 	int temp = *a;
 	*a = *b;
