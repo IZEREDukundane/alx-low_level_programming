103e5bb01cab709328dcaca58447554d9ae09723
IZERE <blaiseinnocenti@tgmail.com>
2024-07-02 19:10:48 -0700

    updates

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,12 @@
  * _strlen - Returns the length of a string.
  * @str: The string to get the length of
  * Return: The length of @str.
- * 
  */
 size_t _strlen(const char *str)
 {
-	size_t length =0;
+	size_t length = 0;
+	
 	while (*str++)
-	length++;
+		length++;
 	return (length);
 }
