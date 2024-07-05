a3e4412507e14327b9a188213ef65646619ee4b4
IZERE <blaiseinnocenti@tgmail.com>
2024-07-04 17:10:29 -0700

    progress

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
-#include "main.h"
 
-/**
- * print_number - prints an integer;
- * @n: integer to be printed;
- */
-void print_number(int n)
-{
-	unsigned int n1;
+// Prototype for the _putchar function
+int _putchar(char c);
 
-	if (n < 0)
-	{
-		n1 = -n;
-		_putchar('-');
-	}
-	else
-	{
-		n1 = n;
-	}
+// Function to print an integer
+void print_number(int n) {
+    // Handle negative numbers
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
 
-	if (n1 / 10)
-		print_number(n1 / 10);
+    // Handle the number printing using recursion
+    if (n / 10 != 0) {
+        print_number(n / 10);
+    }
 
-	_putchar((n1 % 10) + '0');
+    // Print the last digit
+    _putchar((n % 10) + '0');
 }
+
+// Mock implementation of _putchar
+// Replace this with the actual _putchar function
+int _putchar(char c) {
+    return putchar(c);
+}
+
+int main() {
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+
+    return 0;
+}
+
