72e88a0a81a5fa4d8a717f729a95501ce0d4bd2c
IZERE <blaiseinnocenti@tgmail.com>
2024-07-05 12:07:32 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,43 +1,22 @@
-#include <stdio.h>
-
-// Prototype for the _putchar function
-int _putchar(char c);
-
-// Function to print an integer
-void print_number(int n) {
-    // Handle negative numbers
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-
-    // Handle the number printing using recursion
-    if (n / 10 != 0) {
-        print_number(n / 10);
-    }
-
-    // Print the last digit
-    _putchar((n % 10) + '0');
+#include "main.h"
+ 
+/**
+ * print_number - print an integer
+ * @n: parameter, integer value/ input
+ *
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+	
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
 }
-
-// Mock implementation of _putchar
-// Replace this with the actual _putchar function
-int _putchar(char c) {
-    return putchar(c);
-}
-
-int main() {
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
-
-    return 0;
-}
-
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,16 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *rot13(char *);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int _putchar(char c);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_number(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
 
 #endif
