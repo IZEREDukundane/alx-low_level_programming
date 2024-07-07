2fbfd3fc3783ad28f6a7824dc1c91e4474a34bc4
IZERE <blaiseinnocenti@gmail.com>
2024-07-07 04:08:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include  <stdio.h>
-
 /**
- * main - prints "Programming is like building a multilingual puzzle 
- * Return: Always 0.
+ * main - it prints out output using put
+ * Return: Always (0)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
