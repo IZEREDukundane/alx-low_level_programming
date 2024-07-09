2373aec349845a0716deaa2cd344882b651adf5e
IZERE <blaiseinnocenti@gmail.com>
2024-07-09 13:47:50 -0700

    progress

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the number since 0 up to 9
+ * @x: integer to be checked
+ * Return: The number since 0 up to 9
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; X <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
